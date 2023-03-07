85e17b44ff1368a94ffdae151e55caef11c6c8b8
DineshLogan <dineshlogan31@gmail.com>
2023-03-07 10:47:11 +0530

    savedd

--- controller/bookController.js
+++ controller/bookController.js
@@ -1,6 +1,8 @@
 const Author=require("../models/authorModel")
 const Books=require("../models/bookModel")
 const multer=require("multer")
+const fs=require("fs")
+const path=require("path")
 
 const storage=multer.diskStorage({
     destination:(req,file,cb)=>{
@@ -18,9 +20,15 @@ const upload=multer({
 async function getAllBooks(req,res)
 {
 
+  let query=Books.find().lean()
+  if(req.query.title != null && req.query.title != "")
+  {
+    query.regex("title",new RegExp(req.query.title,"i"))
+  }
+
 try {
-  let book= await Books.find({}).lean()
-  console.log(book);
+  let book= await query.exec()
+  // console.log(book);
   res.render("books/index",{book})
 } catch (error) {
   res.redirect("/")
@@ -36,21 +44,54 @@ async function createBook(req,res)
         cover:req.file
 
       }
-      console.log(req.file);
-        console.log(book);
+      // console.log(req.file);
+        // console.log(book);
       try {
         await Books.create(book)
         
         res.redirect("/books")
       } catch  {
-        res.render("books/new")
+        if(book.cover !=null)
+        {
+          removeCover(book.cover)
+        }
+        renderNewPage(res,book,true)
       }
 }
 async function newBook(req,res)
 {
+  renderNewPage(res,new Books())
+  
+}
 
-   let author= await Author.find({}).lean()
-    res.render("books/new",{author})
+async function renderNewPage(res,book,haserror=false)
+{
+  try {
+    let author= await Author.find({}).lean()
+
+    let params={
+      author:author,
+      book:book
+    }
+    if(haserror)
+    {
+      params.errorMessage="Couldn't Create Book"
+    }
+    res.render("books/new",params)
+    
+  } catch  {
+    res.redirect("/")
+  }
+}
+
+function removeCover(cover)
+{
+
+  fs.unlink(path.join("public/uploads",cover.filename),(err)=>{
+    if(err)
+    console.log(err);
+  })
+
 }
 
 module.exports={
